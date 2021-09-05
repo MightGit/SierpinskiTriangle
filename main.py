e2e7b4e158c632e92cbd1e4b3203e2e664ca1cf2
MightGit <74358998+MightGit@users.noreply.github.com>
2021-09-05 18:34:54 +0200

    Problem arise
    Rando_dots not working, needs fixing

---
+++ main.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.bgcolor('blue')
+
+#box, som er vores screen
+def screen():
+   turtle.screensize()
+   turtle.Screen()
+   turtle.done()
+
+rando_dots=[]
+#rando dots er vores 3 punkter for at lave den første trekant
+   def rando_dots(n):
+      if n < 3 
+
+rando_dots()
+
+screen()
+
+
